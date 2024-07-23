d726b3763ddbce1bad566131a19056a83d3813af
IvanovKS <Skitchi@mail.ru>
2024-07-23 21:38:14 +0300

    Add simple header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css';
+import styles from './App.module.css';
+import Header from './components/Header/Header';
 
 function App() {
-  return <div className="App">123</div>;
+  return (
+    <div className={styles.app}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
